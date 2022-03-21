1f8f67ace5ac0978c185eaa0d9a100a2fa4c3088
Tobias Selva <tobiasselva1@gmail.com>
2022-03-20 21:07:34 -0300

    creating a home form

---
+++ src/Components/Home/InputForm.js
@@ -0,0 +1,25 @@
+import { Field, ErrorMessage } from "formik";
+import "../FormStyles.css";
+export default function InputForm({
+  type,
+  id,
+  props,
+  placeholder,
+  value,
+  error,
+}) {
+  return (
+    < >
+      <Field
+        className="input-field"
+        type={type}
+        name={id}
+        value={value}
+        onChange={props.handleChange}
+        placeholder={placeholder}
+      ></Field>
+
+      <ErrorMessage name={id} component={() => <div>{error}</div>} />
+    </>
+  );
+}
