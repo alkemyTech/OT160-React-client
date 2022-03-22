2bdba7493371a7e2a9a623f2f7a57e268e155dc0
gomezdn <gomezdn@outlook.com>
2022-03-22 15:51:18 -0300

    wrapped http requests into an isolated service

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,14 +2,39 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 160                //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+const Get = async (url) => {
+  try {
+    const response = await axios.get(url, config)
+    return response.data
+  }
+
+  catch (error) {
+    console.log(error) // Error handling to be implemented
+  }
 }
 
-export default Get
+const Post = async (url, data) => {
+  try {
+    await axios.post(url, data, config)
+  }
+
+  catch (error) {
+    console.log(error) // Error handling to be implemented
+  }
+}
+
+const Patch = async (url, data) => {
+  try {
+    await axios.patch(url, data, config)
+  } 
+  
+  catch (error) {
+    console.log(error) // Error handling to be implemented
+  }
+}
+
+export { Get, Post, Patch }
