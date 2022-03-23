9162b75f50191a6e44f27ba1841da29b763ad1a9
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-03-23 14:10:10 -0300

    first changes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import Backoffice from "./Components/Backoffice/backoffice";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
+          <Route path="/backoffice" component={Backoffice} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
---
+++ src/Components/Backoffice/backoffice.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Backoffice = () => {
+
+    return (
+        <div className=''>
+            <div className=''>
+                Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+
+            <div className=''>
+                Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+
+            <div className=''>
+            Hey
+            </div>
+        </div>
+    );
+}
+ 
+export default Backoffice;
