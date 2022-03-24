aa3dedc87c4b27eafd4995ab10a66e01b8b29e7b
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-03-24 05:16:55 -0300

    grid layout

--- src/Components/Backoffice/backoffice.js
+++ src/Components/Backoffice/backoffice.js
@@ -1,38 +1,38 @@
 import React from 'react';
-
+import "./backoffice.scss";
 const Backoffice = () => {
 
     return (
-        <div className=''>
-            <div className=''>
+        <div className='container'>
+            <div className='box-1'>
                 Hey
             </div>
 
-            <div className=''>
+            <div className='box-2'>
             Hey
             </div>
 
-            <div className=''>
+            <div className='box-3'>
             Hey
             </div>
 
-            <div className=''>
+            <div className='box-4'>
             Hey
             </div>
 
-            <div className=''>
+            <div className='box-5'>
                 Hey
             </div>
 
-            <div className=''>
+            <div className='box-6'>
             Hey
             </div>
 
-            <div className=''>
+            <div className='box-7'>
             Hey
             </div>
 
-            <div className=''>
+            <div className='box-8'>
             Hey
             </div>
         </div>
