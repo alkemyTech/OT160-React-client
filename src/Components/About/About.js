76ef1e42fbd03fdec1c9d693e35824ef955c6525
gomezdn <gomezdn@outlook.com>
2022-03-24 04:03:06 -0300

    added temporary filler for "about us" text and styled basic layout

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,11 +6,22 @@ import Title from './Title'
 import AboutUs from './AboutUs'
 
 function About() {
-  const aboutUsContent = 'temporary filler' // to be retrieved later from the API service
-
+  const aboutUsContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                          Morbi nunc tellus, condimentum pellentesque nunc in, volutpat
+                          laoreet dui. Aliquam metus ipsum, iaculis in imperdiet eu,
+                          consequat ac purus. Nam sed placerat nibh. Donec aliquet est vel
+                          ipsum interdum, ultricies pretium risus semper. Lorem ipsum dolor
+                          sit amet, consectetur adipiscing elit. Proin id nibh ac mi ullamcorper
+                          maximus in vitae leo. Aenean ac ultrices quam. Nullam vel sagittis magna.
+                          Phasellus tristique libero vitae molestie consequat. In quis magna at velit
+                          commodo commodo in quis orci. Vivamus facilisis varius est sed dictum.
+                          Sed massa est, luctus a pretium molestie, lacinia ac justo. Donec aliquam
+                          ante nec tempor vehicula. Donec in leo mi. `
+                          // hardcoded now for presentation, to be retrieved later from an API service
+                          
   return (
-    <Container as='section'>
-      <Stack className='align-items-center'>
+    <Container as='section' className='mt-3'>
+      <Stack className='align-items-center' gap='5'>
         <Title content='Nosotros'/>
         <AboutUs content={aboutUsContent}/>
       </Stack>
