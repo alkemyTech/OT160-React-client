76900f91d42b169072b10cd6c7c86b043596d994
Tobias Selva <tobiasselva1@gmail.com>
2022-03-26 20:44:41 -0300

    adding react-bootstrap package

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import HomeForm from './Components/Home/HomeForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import Donation from "./Components/Donations/Donation";
+import Thanks from "./Components/Donations/Thanks";
+import HomeForm from "./Components/Home/HomeForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
 
 function App() {
   return (
@@ -28,59 +30,68 @@ function App() {
           <Route path="/backoffice/organization" component={HomeForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
+          <Route
+            path="/donate"
+            component={(text) => {
+              text = "Welcome to the Donate section, click the MercadoPago button to make your donation to our ONG";
+              return <Donation text={text} />;
+            }}
+          />
+          <Route path="/thanks" component={Thanks} />
+          <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
         </Switch>
       </BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Counter />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <span>
+            <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+            </a>
+            ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+            </a>
+          </span>
+        </header>
+      </div>
     </>
   );
 }
