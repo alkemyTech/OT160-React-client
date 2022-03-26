ca2a3dd6fe78e3c54792cbca2eee0a2e8d7fab73
gomezdn <gomezdn@outlook.com>
2022-03-25 21:02:18 -0300

    added semicolons and corrected indentation

--- src/Services/formValidationsService.js
+++ src/Services/formValidationsService.js
@@ -1,22 +1,22 @@
-const VALID_IMAGE_FORMATS = ['jpg', 'png']
+const VALID_IMAGE_FORMATS = ['jpg', 'png'];
 
 const imageValidator = {
   getFormat: (image) => {
-    const splitFilePath = image.split('.')
-    const extension = splitFilePath[splitFilePath.length -1]
-    return extension?.toLowerCase()
+    const splitFilePath = image.split('.');
+    const extension = splitFilePath[splitFilePath.length -1];
+    return extension?.toLowerCase();
   },
   isValid: (imageFormat) => {
-    return VALID_IMAGE_FORMATS.includes(imageFormat)
+    return VALID_IMAGE_FORMATS.includes(imageFormat);
   },
   formatError: `El formato del archivo debe ser uno de los siguientes:  ${VALID_IMAGE_FORMATS.join(', ')}.`
-}
+};
 
 const emailValidator = {
   isValid: (email) => {
-    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
   },
   error: 'Ingrese un email válido por favor.'
-}
+};
 
 export { imageValidator, emailValidator }
--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -4,34 +4,31 @@ const config = {
     headers: {
         Group: 160
     }
-}
+};
 
 const get = async (url) => {
   try {
-    const response = await axios.get(url, config)
-    return response.data
-  }
-  catch (error) {
-    console.log(error) // Error handling to be implemented
-  }
-}
+    const response = await axios.get(url, config);
+    return response.data;
+  } catch (error) {
+     console.log(error) // Error handling to be implemented
+  };
+};
 
 const post = async (url, data) => {
   try {
-    await axios.post(url, data, config)
-  }
-  catch (error) {
-    console.log(error) // Error handling to be implemented
-  }
-}
+    await axios.post(url, data, config);
+  } catch (error) {
+      console.log(error) // Error handling to be implemented
+  };
+};
 
 const patch = async (url, data) => {
   try {
-    await axios.patch(url, data, config)
-  } 
-  catch (error) {
-    console.log(error) // Error handling to be implemented
-  }
-}
+    await axios.patch(url, data, config);
+  } catch (error) {
+      console.log(error) // Error handling to be implemented
+  };
+};
 
 export { get, post, patch }
