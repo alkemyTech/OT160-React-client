82c53271dd4adf858322cf6eb1e6db9f9591f2d1
Tobias Selva <tobiasselva1@gmail.com>
2022-03-26 22:07:31 -0300

    debug

--- src/Components/Donations/Thanks.js
+++ src/Components/Donations/Thanks.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Thanks()
 {
     return(
-        <div>
+        <div style={{textAlign:"center"}}>
             <h1>Thanks</h1>
             <h2>Thanks for make a donation to our ONG :)</h2>
         </div>
