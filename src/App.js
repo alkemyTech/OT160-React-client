bed18d086c4c5ecee8f43ca9d2ea95288c75a9e2
gomezdn <gomezdn@outlook.com>
2022-03-26 22:01:04 -0300

    updated route name and deleted unnecessary component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/nosotros" component={About}/>
+          <Route path="/about" component={About}/>
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,28 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Stack from 'react-bootstrap/Stack'
 import Container from 'react-bootstrap/Container'
-import AboutUs from './AboutUs'
 
 function About() {
-  let aboutUsContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Morbi nunc tellus, condimentum pellentesque nunc in, volutpat
-                        laoreet dui. Aliquam metus ipsum, iaculis in imperdiet eu,
-                        consequat ac purus. Nam sed placerat nibh. Donec aliquet est vel
-                        ipsum interdum, ultricies pretium risus semper. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Proin id nibh ac mi ullamcorper
-                        maximus in vitae leo. Aenean ac ultrices quam. Nullam vel sagittis magna.
-                        Phasellus tristique libero vitae molestie consequat. In quis magna at velit
-                        commodo commodo in quis orci. Vivamus facilisis varius est sed dictum.
-                        Sed massa est, luctus a pretium molestie, lacinia ac justo. Donec aliquam
-                        ante nec tempor vehicula. Donec in leo mi. `
-                        // Hardcoded for presentation but it's gonna be retrieved later from an API service
+  const [aboutUs, setAboutUs] = useState('')
+
+  useEffect(() => {
+    setAboutUs(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Morbi nunc tellus, condimentum pellentesque nunc in, volutpat
+                laoreet dui. Aliquam metus ipsum, iaculis in imperdiet eu,
+                consequat ac purus. Nam sed placerat nibh.`)
+  },[])
   
   return (
     <Container as='section' className='mt-3'>
       <Stack className='align-items-center' gap='4'>
         <h1>Nosotros</h1> {/** This title is gonna be a component made in another ticket**/}
-        <AboutUs content={aboutUsContent}/>
+        <Stack className='align-items-center'>    
+          <h4>Sobre nosotros</h4>
+          <p className='text-left w-75'>{aboutUs}</p>
+        </Stack>
       </Stack>
     </Container>
   )
