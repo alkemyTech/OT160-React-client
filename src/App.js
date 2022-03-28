baf1972731c4d316d890231b1bc17988a361c85f
rodrigo <rodrigoespinosa24@gmail.com>
2022-03-27 22:35:01 -0300

    borrar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
-import Title from './Components/Title/Title';
 
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={Title} />
+          <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
