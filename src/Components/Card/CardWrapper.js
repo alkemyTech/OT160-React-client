c3dd4c4198a502b672f853e5454ddb245b15d544
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-03-28 10:09:24 -0300

    CardWrapper component created

---
+++ src/Components/Card/CardWrapper.js
@@ -0,0 +1,17 @@
+import Card from "../Card/Card";
+import "../Card/card.scss";
+
+
+const CardWrapper = ({cardsObject}) => {
+    const cards = cardsObject.map((item) => {
+        return <Card title={item.title} img={item.img === "" ? "/images/placeholder/empty-image.png" : item.img} description={item.description}/>
+    });
+
+    return (
+        <div className="wrapper-container"> 
+           {cards} 
+        </div>
+        );
+};
+
+export default CardWrapper;
