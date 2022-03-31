2550106a7622040aa49b72117cb88ba09f94a87a
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-03-31 14:15:41 -0300

    useApiService file added

---
+++ src/Services/userApiService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'; 
+
+const putUser = (userData) => { 
+    axios({ 
+        method: "PUT", 
+        url: "https://ongapi.alkemy.org/api/users/" + userData.id, 
+        data: userData 
+    }); 
+} 
+
+const postUser = (userData) =>{ 
+    axios({ method: "POST", 
+    url: "https://ongapi.alkemy.org/api/users", 
+    data: userData 
+    }); 
+} 
+
+export {putUser, postUser}
