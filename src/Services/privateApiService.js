3e8d67343e5c6116892c5627800e8686845b68b4
Tobias Selva <tobiasselva1@gmail.com>
2022-03-31 21:37:40 -0300

    creating headerAuth

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
+  headers: {
+    Group: 160, //Aqui va el ID del equipo!!
+  },
+};
+
+
+const getToken=()=>
+{
+	return JSON.parse(localStorage.getItem("token"));
 }
+export const HeaderAuthorization = () => {
+  const token = getToken();
+  const headerAuthorization = {Bearer: ""};
+  if (token !== null) {
+    headerAuthorization.Bearer="Bearer: "+ token;
+  }
+  return headerAuthorization;
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get("https://jsonplaceholder.typicode.com/users", config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export default Get;
