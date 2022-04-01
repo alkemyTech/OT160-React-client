e06dc9ff3de654380b79272f87f54ceaeb20016e
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-01 09:50:07 -0300

    readability fixes

--- src/Services/formValidation.js
+++ src/Services/formValidation.js
@@ -13,33 +13,38 @@ const lastnameValidation = (lastName, errors) => {
 }
 
 const emailValidation = (email, errors) => {
-    if (!email) {
-      errors.email = "Requerido";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-      errors.email = "Email inválido";
-    }
+  if (!email) {
+    errors.email = "Requerido";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+    errors.email = "Email inválido";
+  }
   return errors;
 };
         
 const passwordValidationEightLength = (password, errors) => {
-    if (!password) {
-      errors.password = "Requerido";
-      } else if (password.length < 7){
-        errors.password = "La contraseña debe tener un mínimo de 6 characteres";
-      }else if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/i.test(password)) {
-        errors.password = "La contraseña debe tener al menos una letra, un número y un caracter especial";
-      }
-
+  if (!password) {
+    errors.password = "Requerido";
+  } else if (password.length < 7){
+    errors.password = "La contraseña debe tener un mínimo de 6 characteres";
+  }else if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/i.test(password)) {
+    errors.password = "La contraseña debe tener al menos una letra, un número y un caracter especial";
+  }
   return errors;
 };
 
 const confirmedPasswordValidation = (password, confirmedPassword, errors) => {
-    if (!confirmedPassword) {
-      errors.confirmedPassword = "Confirme su contraseña";
-      } else if (confirmedPassword !== password){
-        errors.confirmedPassword = "La contraseña no coincide, por favor inténtelo de nuevo";
-      } 
+  if (!confirmedPassword) {
+    errors.confirmedPassword = "Confirme su contraseña";
+  } else if (confirmedPassword !== password){
+    errors.confirmedPassword = "La contraseña no coincide, por favor inténtelo de nuevo";
+  } 
   return errors;
 };
 
-export {emailValidation, passwordValidationEightLength, confirmedPasswordValidation, nameValidation, lastnameValidation}      
+export {
+  emailValidation, 
+  passwordValidationEightLength, 
+  confirmedPasswordValidation, 
+  nameValidation, 
+  lastnameValidation
+}      
