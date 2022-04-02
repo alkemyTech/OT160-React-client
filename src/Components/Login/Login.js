75c20e467fa7fcb73b04bef4d1bc4995c12cd6c7
gomezdn <gomezdn@outlook.com>
2022-04-02 06:58:13 -0300

    fixed format

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,54 +1,58 @@
-import React, { useState } from "react";
-import "../FormStyles.css";
-import "../Login/LoginStyles.css";
+import React, {useState} from 'react';
+import '../FormStyles.css';
+import '../Login/LoginStyles.css'
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
+
 function Login() {
-  const [valuesForm, setValuesForm] = useState({});
 
-  const validate = (values) => {
-    const errores = {};
-    validateEmail(values, errores);
-    validatePassword(values, errores);
+    const [valuesForm , setValuesForm] = useState({});
 
-    return errores;
-  };
+    const validate = values => {
+      const errores = {};
+      validateEmail(values, errores);
+      validatePassword(values, errores)
 
-  const validateEmail = (values, errores) => {
-    if (!values.email) {
-      errores.email = "Please enter a email";
-    } else if (
-      !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)
-    ) {
-      errores.email =
-        "The email can only have letters, numbers, periods, hyphens and underscore";
-    }
-  };
-
-  const validatePassword = (values, errores) => {
-    if (!values.password) {
-      errores.password = "Please enter a password";
-    } else if (
-      !/^(?=(?:.*\d))(?=.*[A-z])(?=.*[.,*!?¿¡/#$%&])\S{6,25}$/.test(
-        values.password
-      )
-    ) {
-      errores.password =
-        "The password must have a minimum length of 6 characters, and contain at least one number, one letter and one symbol";
-    }
-  };
-
-  return (
-    <div className="form-login">
-      <Formik
+      return errores;
+    };
+    
+    const validateEmail = (values, errores) => {
+      if (!values.email) {
+        errores.email = "Please enter a email";
+      } else if (
+        !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+          values.email
+        )
+      ) {
+        errores.email =
+          "The email can only have letters, numbers, periods, hyphens and underscore";
+      }
+    };
+    
+    const validatePassword = (values, errores) => {
+      if (!values.password) {
+        errores.password = "Please enter a password";
+      } else if (
+          !/^(?=(?:.*\d))(?=.*[A-z])(?=.*[.,*!?¿¡/#$%&])\S{6,25}$/.test(values.password)) 
+          {
+        errores.password =
+          "The password must have a minimum length of 6 characters, and contain at least one number, one letter and one symbol";
+      }
+    };
+    
+    return (
+        <div className='form-login'>
+        <Formik
         initialValues={{
           email: "",
           password: "",
         }}
+
         validate={validate}
+
         onSubmit={(values, { resetForm }) => {
           resetForm();
-          setValuesForm(values);
+          setValuesForm(values)
         }}
       >
         {({ errors }) => (
@@ -64,10 +68,12 @@ function Login() {
                 name="email"
                 placeholder="name@example.com"
               />
-
+              
               <ErrorMessage
                 name="email"
-                component={() => <div className="">{errors.email}</div>}
+                component={() => (
+                  <div className="">{errors.email}</div>
+                )}
               />
             </div>
             <div className="">
@@ -78,22 +84,27 @@ function Login() {
                 name="password"
                 placeholder="Password"
               />
-
-              <ErrorMessage
-                name="password"
-                component={() => <div className="">{errors.password}</div>}
-              />
+             
+            <ErrorMessage
+              name="password"
+              component={() => (
+                <div className="">{errors.password}</div>
+              )}
+            />
             </div>
             <div className="">
-              <button className="button-login" type="botton">
+              <button
+                className="button-login"
+                type="botton"
+              >
                 Send
               </button>
             </div>
           </Form>
         )}
       </Formik>
-    </div>
-  );
+      </div>
+    );
 }
 
 export default Login;
