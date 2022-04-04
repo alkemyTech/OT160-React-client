230e3d43a31972b1ffcdcc74ca7b46a08034a307
Tobias Selva <tobiasselva1@gmail.com>
2022-04-04 20:24:28 -0300

    change donation css to scss

--- src/Components/Donations/DonationCard.js
+++ src/Components/Donations/DonationCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import logo from '../../assets/logo.png';
-import './donation.css';
+import './donations.scss';
 
 export default function DonationCard({
   title,
