f5da37d971baf0213200df77b0f569ef7a171747
rodrigo <rodrigoespinosa24@gmail.com>
2022-04-04 22:04:51 -0300

    add identation

--- src/Components/Activities/Error.js
+++ src/Components/Activities/Error.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import { Alert } from 'react-bootstrap';
 
-export default function AlertDismissibleExample({message}) {
+export default function AlertDismissibleExample({ message }) {
 
     const [show, setShow] = useState(true);
 
     return (
-        
+
         <>
             { show && (
                 <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                    <Alert.Heading> {message} </Alert.Heading>
+
+                    <Alert.Heading> { message } </Alert.Heading>
                     
                 </Alert>)
             }
