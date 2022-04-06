0f119ba2c4b6a3ee5d98a194ec02e40d7c486c27
facundo diaz <facundodiaz25@gmail.com>
2022-04-06 10:06:48 -0300

    add component slides and NewList

--- src/Components/Home/Index.js
+++ src/Components/Home/Index.js
@@ -1,27 +1,34 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import Slides from '../Slides/SlidesForm'
+import NewsList from "../News/NewsList";
 
 function index() {
   return (
-    <div className="my-3 container-fluid">
+    <div className="mt-3 container-xxl">
       <Col>
         <Row>
-          <div className="w-100 text-center">
+          <div className="w-100">
             <h1 className="fs-1 text-center">Bienvenido a Somos Mas</h1>
           </div>
         </Row>
         <Row>
-          <div className='text-center placeholder-glow"'>
+          <div className='text-center mt-3 mb-3 container-xl'>
             <p className="fs-3">Ultimas Novedades</p>
-            <div className="card">
-            <p class="placeholder-wave">
-                <span class="placeholder col-12">esto es un placeholder</span>
-            </p>
+            <div className="row justify-content-center">
+              <NewsList />
             </div>
           </div>
         </Row>
-        <Row></Row>
+        <Row>
+          <div className="text-center mt-3 mb-3 container-xl">
+            <p className="fs-3">Slides form</p>
+            <div className="row justify-content-center">
+                <Slides/>
+            </div>
+          </div>
+        </Row>
       </Col>
     </div>
   );
