4ad30063cba39fe3d53346da8d8d9c6698ac0109
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-07 06:06:19 -0300

    getCardsList function created

--- src/Components/Card/CardWrapper.js
+++ src/Components/Card/CardWrapper.js
@@ -4,17 +4,19 @@ import "../Card/card.scss";
 const CardWrapper = ({cardsList}) => {
     const DEFAULT_IMAGE = "/images/placeholder/empty-image.png";
 
-    const cards = cardsList.map((item) => {
-        return <Card 
-                    title={item.title} 
-                    img={item.img || DEFAULT_IMAGE} 
-                    description={item.description}
-                />
-    });
+    const getCardsList = () =>{
+        return cardsList.map((item) => {
+                return <Card 
+                        title={item.title} 
+                        img={item.img || DEFAULT_IMAGE} 
+                        description={item.description}
+                    />
+                });
+    } 
 
     return (
         <div className="wrapper-container"> 
-           {cards} 
+           {getCardsList()} 
         </div>
         );
 };
