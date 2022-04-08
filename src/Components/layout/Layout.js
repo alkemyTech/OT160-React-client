0b1e47e690ef6e913b59ad4191260cc35923334f
rodrigo <rodrigoespinosa24@gmail.com>
2022-04-07 22:51:58 -0300

    resolved con

--- src/Components/layout/Layout.js
+++ src/Components/layout/Layout.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-
-
 export default function Layout(props) {
 
     const content = ["navbar", "footer"];
+
     return (
         <>
             {content[0]}
 
             {props.children}
-            
+
             {content[1]}
         </>
     )
