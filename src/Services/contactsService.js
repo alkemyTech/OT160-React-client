312401ff71242157451bc5ff3a938a201b57c156
gomezdn <gomezdn@outlook.com>
2022-04-10 15:33:41 -0300

    deleted unneeded variable

--- src/Services/contactsService.js
+++ src/Services/contactsService.js
@@ -4,18 +4,15 @@ const BASE_URL = 'https://ongapi.alkemy.org/docs';
 const BASE_ENDPOINT = '/contacts';
 
 async function getContacts() {
-  const response = await get(`${BASE_URL}${BASE_ENDPOINT}`);
-  return response;
+  return await get(`${BASE_URL}${BASE_ENDPOINT}`);
 }
 
 async function getContact(id) {
-  const response = await get(`${BASE_URL}${BASE_ENDPOINT}/:${id}`);
-  return response;
+  return await get(`${BASE_URL}${BASE_ENDPOINT}/:${id}`);
 }
 
 async function createContact(data) {
-  const response = await post(`${BASE_URL}${BASE_ENDPOINT}`, data);
-  return response;
+  return await post(`${BASE_URL}${BASE_ENDPOINT}`, data);
 }
 
-export { createContact, getContacts, getContact };
+export { getContacts, getContact, createContact };
