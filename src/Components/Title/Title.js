1539f3683aba293525830f5e5b932f58cf3bb1f7
Tobias Selva <tobiasselva1@gmail.com>
2022-04-10 21:24:23 -0300

    fix

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import '../Title/title.css'
-import { imagen_title_default } from '../../utility/constUtility'
+import { DEFAULT_IMAGE_FOR_TITLE } from '../../utility/imagesUtility'
+import '../Title/title.scss'
 
-
-export default function Title({title,image=imagen_title_default}) {
+export default function Title({ title, image = DEFAULT_IMAGE_FOR_TITLE }) {
 
   return (
-    <div style={{backgroundImage:`url(${image})` }}>
+    <div style = {{ backgroundImage:`url(${image})` }}>
 
-      <h1 className="title">{title}</h1>
+      <h1 className="align-text-center"> {title} </h1>
       
     </div>
   )
