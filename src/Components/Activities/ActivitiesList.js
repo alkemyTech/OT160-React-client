ce4c4e95bfe086c33743dfaf7aa6aaecad02f50e
facundo diaz <facundodiaz25@gmail.com>
2022-04-11 15:29:55 -0300

    add botons in the table

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,67 +1,102 @@
-import React from 'react';
-import {Table} from 'react-bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css" ;
-import '../CardListStyles.css';
+import React, { useState } from "react";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  FormGroup,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const ActivitiesList = () => {
-    const activitiesMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
-    ];
+  const activitiesMock = [
+    {
+      id: 1,
+      name: "Titulo de prueba",
+      image: "Descripcion de prueba",
+      createdAt: "Descripcion de prueba",
+    },
+    {
+      id: 2,
+      name: "Titulo de prueba",
+      image: "Descripcion de prueba",
+      createdAt: "Descripcion de prueba",
+    },
+    {
+      id: 3,
+      name: "Titulo de prueba",
+      image: "Descripcion de prueba",
+      createdAt: "Descripcion de prueba",
+    },
+  ];
 
-    return (
-        <div>
-            <h1>Listado Actividades</h1>
-            <ul className="list-container">
-                {activitiesMock.length > 0 ?
-                    activitiesMock.map((activity) => {
-                        return(
-                            <li className="card-info" key={activity.id}>
-                                <h3>{activity.name}</h3>
-                                <p>{activity.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay actividades</p>
-                }
-            </ul>
-            <div className='container-xl mt-4'>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Image</th>
-                    <th>CreateAt</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td><button>Editar</button></td>
-                    <td><button>Eliminar</button></td>
-                    </tr>
-                    <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
-</Table>
-            </div>
-        </div>
-    );
-}
- 
+  const [dataTable, setDataTable] = useState(activitiesMock);
+
+  return (
+    <div className="mt-2 container-xxl">
+      <h1 className="text-center">Listado Actividades</h1>
+      <div className="container-xl mt-4">
+        <Button className="btn-success">Nueva Actividad</Button>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>CreateAt</th>
+              <th>Accions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataTable.map((element) => (
+              <tr>
+                <td>{element.id}</td>
+                <td>{element.name}</td>
+                <td>{element.image}</td>
+                <td>{element.createdAt}</td>
+                <td>
+                  <button className="btn btn-secondary">Editar</button>
+                  {"  "}
+                  <button className="btn btn-danger">Eliminar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Modal>
+            <ModalHeader>
+                <div>
+                    <h3>Editar Actividad</h3>
+                </div>
+            </ModalHeader>
+            <ModalBody>
+               <FormGroup>
+                   <label>Id:</label>
+                   <input className="form-control" readOnly type="text"/>
+               </FormGroup>
+               <FormGroup>
+                   <label>Name</label>
+                   <input className="form-control" readOnly type="text"/>
+               </FormGroup>
+               <FormGroup>
+                   <label>Image</label>
+                   <input className="form-control" readOnly type="text"/>
+               </FormGroup>
+               <FormGroup>
+                   <label>CreatedAt</label>
+                   <input className="form-control" readOnly type="text"/>
+               </FormGroup>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary">Confirmar</Button>
+                <Button color="danger">Cancelar</Button>
+            </ModalFooter>
+        </Modal>
+      </div>
+    </div>
+  );
+};
+
 export default ActivitiesList;
