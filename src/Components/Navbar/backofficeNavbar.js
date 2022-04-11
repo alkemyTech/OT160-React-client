09c29659d28cd7150d020d0c7e725127a6caa6ed
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-11 13:32:44 -0300

    backoffice header button created

--- src/Components/Navbar/backofficeNavbar.js
+++ src/Components/Navbar/backofficeNavbar.js
@@ -1,21 +1,25 @@
+import React  from "react";
+import { Link } from "react-router-dom";
 
-const BackofficeNavBar = () => {
-    
+const BackofficeNavBar = () => {   
     return(
-            <nav class="navbar navbar-light bg-light">
-              <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-              </div>
-              <div class="collapse" id="navbarToggleExternalContent">
-              <div class="bg-light p-4">
-                <h5 class="text-white h4">Collapsed content</h5>
-                <span class="text-muted">Toggleable via the navbar brand.</span>
+        <nav class="navbar-light bg-light d-flex justify-content-around align-items-start">
+          <div>
+            <div class="container-fluid">
+              <button class="navbar-toggler shadow-none mt-2 " type="button">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+            <div>
+              <div class="items">
+                <Link to="/backoffice">
+                Some item
+                </Link>
               </div>
             </div>
-            </nav>
+          </div>
+        </nav>
     )
-}
+};
 
 export default BackofficeNavBar;
