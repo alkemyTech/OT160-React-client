bf2c01f0c21e2af04746a45120c40fd8c407c734
Tobias Selva <tobiasselva1@gmail.com>
2022-04-10 21:52:31 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
+import Register from './Components/Auth/RegisterForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
 import SlidesForm from './Components/Slides/SlidesForm';
-import Donation from './Components/Donations/Donation';
-import Thanks from './Components/Donations/Thanks';
 import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import UserForm from './Components/Users/UsersForm';
 import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import About from './Components/About/About'
+import Backoffice from "./Components/Backoffice/backoffice";
 import Login from './Components/Login/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import UsersList from './Components/Users/UsersList';
 
 function App() {
   return (
@@ -24,21 +23,16 @@ function App() {
       <BrowserRouter>
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+          <Route path="/register" component={Register} />
+          <Route path="/about" component={About}/>
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
+          <Route path="/backoffice" component={Backoffice} />
           <Route path="/backoffice/Slides" component={SlidesForm} />
+          <Route exact path="/backoffice/users" component={UsersList}/>
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
-          <Route
-            path="/donate"
-            component={() => {
-                const welcomeText = "Bienvenido a la seccion de donacines.";
-
-              return <Donation text={welcomeText} />;
-            }}
-          />
-          <Route path="/thanks" component={Thanks} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
@@ -46,6 +40,7 @@ function App() {
           <Route path="/login" component={Login} />
         </Switch>
       </BrowserRouter>
+    
     </>
   );
 }
