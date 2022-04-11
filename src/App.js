2f49866bbce4ae320fae53532ce0c55f8926869d
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-11 06:32:58 -0300

    main router functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React,{ Suspense } from 'react';
+import {BrowserRouter, Switch} from 'react-router-dom';
+import PublicRoute from './routes/PublicRoute'; 
+import PrivateRoute from './routes/PrivateRoute';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import Register from './Components/Auth/RegisterForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
@@ -18,12 +19,57 @@ import Login from './Components/Login/Login';
 import UsersList from './Components/Users/UsersList';
 
 function App() {
+  const isAuthenticated = false;
+
   return (
     <>
       <BrowserRouter>
-        <Switch>
+        <Suspense fallback={<div>Loading</div>}>
+          <Switch>
+            <PublicRoute path="/register" isAuthenticated={isAuthenticated}>
+              <Register />
+            </PublicRoute>
+            <PublicRoute path="/about" isAuthenticated={isAuthenticated}>
+              <About />
+            </PublicRoute>
+            <PublicRoute path="/create-activity" isAuthenticated={isAuthenticated}>
+              <ActivitiesForm />
+            </PublicRoute>
+            <PublicRoute path="/create-category" isAuthenticated={isAuthenticated}>
+              <CategoriesForm />
+            </PublicRoute>
+            <PublicRoute path="/create-news" isAuthenticated={isAuthenticated}>
+              <NewsForm />
+            </PublicRoute>
+            <PublicRoute path="/create-testimonials" isAuthenticated={isAuthenticated}>
+              <TestimonialForm />
+            </PublicRoute>
+            <PublicRoute path="/create-user" isAuthenticated={isAuthenticated}>
+              <UserForm />
+            </PublicRoute>
+            <PublicRoute path="/create-member" isAuthenticated={isAuthenticated}>
+              <MembersForm />
+            </PublicRoute>
+            <PublicRoute path="/school-campaign" isAuthenticated={isAuthenticated}>
+              <SchoolCampaign />
+            </PublicRoute>
+            <PublicRoute path="/toys-campaign" isAuthenticated={isAuthenticated}>
+              <ToysCampaign />
+            </PublicRoute>
+            <PublicRoute path="/login" isAuthenticated={isAuthenticated}>
+              <Login />
+            </PublicRoute>
+            <PrivateRoute path="/backoffice" isAuthenticated={isAuthenticated}>
+              <Backoffice />
+            </PrivateRoute>
+            <PrivateRoute path="/backoffice/slides" isAuthenticated={isAuthenticated}>
+              <SlidesForm />
+            </PrivateRoute>
+            <PrivateRoute path="/backoffice/users" isAuthenticated={isAuthenticated}>
+              <UsersList />
+            </PrivateRoute>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/register" component={Register} />
+          {/* <Route path="/register" component={Register} />
           <Route path="/about" component={About}/>
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
@@ -37,8 +83,9 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/login" component={Login} />
-        </Switch>
+          <Route path="/login" component={Login} /> */}
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     
     </>
---
+++ src/routes/PublicRoute.js
@@ -0,0 +1,12 @@
+import {Route, Redirect} from 'react-router-dom';
+  
+const PublicRoute = ({ children, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        render={() => children}
+      />
+    );
+}
+
+export default PublicRoute;
