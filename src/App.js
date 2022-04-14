e70f330b6bcef7c33f98dade3c97b7b4fb464ef5
Tobias Selva <tobiasselva1@gmail.com>
2022-04-13 22:02:54 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import ActivityDetails from './Components/Activities/ActivityDetails/ActivityDetails';
-import Register from './Components/Auth/RegisterForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import About from './Components/About/About'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import ActivityDetails from "./Components/Activities/ActivityDetails/ActivityDetails";
+import Register from "./Components/Auth/RegisterForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import Donation from "./Components/Donations/Donation";
+import Thanks from "./Components/Donations/Thanks";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import About from "./Components/About/About";
 import Backoffice from "./Components/Backoffice/backoffice";
-import EditOrganization from './Components/Organization/EditOrganization';
-import Login from './Components/Login/Login';
-import UsersList from './Components/Users/UsersList';
-
+import EditOrganization from "./Components/Organization/EditOrganization";
+import Login from "./Components/Login/Login";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
   return (
@@ -27,17 +28,29 @@ function App() {
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path="/register" component={Register} />
-          <Route path="/about" component={About}/>
+          <Route path="/about" component={About} />
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/activities/:id" component={ActivityDetails} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route exact path="/backoffice" component={Backoffice} />
           <Route path="/backoffice/Slides" component={SlidesForm} />
-          <Route path="/backoffice/organization/edit" component={EditOrganization} />
-          <Route exact path="/backoffice/users" component={UsersList}/>
+          <Route
+            path="/backoffice/organization/edit"
+            component={EditOrganization}
+          />
+          <Route exact path="/backoffice/users" component={UsersList} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
+          <Route
+            path="/donate"
+            component={
+            () => {
+              const welcomeText = "Bienvenido a la seccion de donacines.";
+              return <Donation text={welcomeText} />;
+            }}
+          />
+          <Route path="/thanks" component={Thanks} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
