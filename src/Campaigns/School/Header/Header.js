ce7c38e6df4bc15a672ec87eb8615f4a81db8c97
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-15 10:31:35 -0300

    responsive layout

--- src/Campaigns/School/Header/Header.js
+++ src/Campaigns/School/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./header.scss";
+import ongLogoImg from "../../../icons/somos-mas-logotype.png";
 
 const toysLogoImg = "/images/campaigns/toys/toys-logotype.png";
+const toysSlogan = "/images/campaigns/toys/toys-slogan.png";
 
 const Header = () => {
   return (
     <div className='header border-bottom'>
-      <img className="header__logo" alt='logo' src={toysLogoImg} />
+      <img src={toysSlogan} alt="campaign slogan" className='header__slogan'/>
+      <img className="header__campaign-logo" alt='campaign logo' src={toysLogoImg} />
+      <img src={ongLogoImg} alt="somos más logo" className='header__ong-logo'/>
     </div>
   );
 }
