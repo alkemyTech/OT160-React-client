1478ae037e4842f86c349de649f8781a87706c5e
rodrigo <rodrigoespinosa24@gmail.com>
2022-04-16 18:35:44 -0300

    modificando componentes

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from "../../icons/user/LOGO-SOMOS MAS.png";
 import Icon from './Icon';
-import "./footer.scss";
 import Links from './Links';
-export default function Footer() {
-  const instagram = "INSTAGRAM"
+import "./footer.scss";
+
+export default function Footer() {  
   return (
     <>
       <div className='footer'>
         <div className='row bg-black'>
-          <div className='col-lg-2'><Links/></div>
-          <div className='col-lg-2'><Links/></div>
+          <div className='col-lg-2'><Links link={"LINK"}/></div>
+          <div className='col-lg-2'><Links link={"LINK"}/></div>
           <div className='col-lg-4'>
             <div className='d-flex justify-content-center flex-md-column align-items-md-center'>
               <img src={logo} className="img position-absolute" style={{ top: -10 }} alt="logo"></img>
               <p className='text-white d-none d-md-block mb-0 mt-5'>SOMOS MAS SITE</p>
             </div>
           </div>
-          <div className='col-lg-2'><Links/></div>
-          <div className='col-lg-2'><Links/></div>
+          <div className='col-lg-2'><Links link={"LINK"}/></div>
+          <div className='col-lg-2'><Links link={"LINK"}/></div>
           <hr className='hr d-none d-xl-block'/>
           <div className='icon d-flex justify-content-around mt-5 mt-md-2 mb-1 mb-md-3'>
             <Icon name={"instagram"} />
