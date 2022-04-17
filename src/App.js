f6d7fced7160382d23477b64a5cdb16cbbd633e2
Tobias Selva <tobiasselva1@gmail.com>
2022-04-16 23:28:15 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import Register from "./Components/Auth/RegisterForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import About from "./Components/About/About";
-import Backoffice from "./Components/Backoffice/backoffice";
-import EditOrganization from "./Components/Organization/EditOrganization";
-import Login from "./Components/Login/Login";
-import UsersList from "./Components/Users/UsersList";
-import ActivitiesList from "./Components/Activities/ActivitiesList";
-import { spring, AnimatedSwitch } from "react-router-transition";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ActivitiesForm from './Components/Activities/ActivitiesForm';
+import Register from './Components/Auth/RegisterForm';
+import CategoriesForm from './Components/Categories/CategoriesForm';
+import NewsForm from './Components/News/NewsForm';
+import SlidesForm from './Components/Slides/SlidesForm';
+import TestimonialForm from './Components/Testimonials/TestimonialsForm';
+import UserForm from './Components/Users/UsersForm';
+import SchoolCampaign from './Campaigns/School/SchoolCampaign';
+import ToysCampaign from './Campaigns/Toys/ToysCampaign';
+import MembersForm from './Components/Members/MembersForm';
+import ProjectsForm from './Components/Projects/ProjectsForm';
+import About from './Components/About/About';
+import Backoffice from './Components/Backoffice/backoffice';
+import EditOrganization from './Components/Organization/EditOrganization';
+import Login from './Components/Login/Login';
+import UsersList from './Components/Users/UsersList';
+import Home from './Components/Home';
+import ActivitiesList from './Components/Activities/ActivitiesList';
+import { spring, AnimatedSwitch } from 'react-router-transition';
+
 function App() {
-  // we need to map the `scale` prop we define below
-  // to the transform style property
   function mapStyles(styles) {
     return {
       opacity: styles.opacity,
@@ -37,24 +37,22 @@ function App() {
     });
   }
 
-  // child matches will...
   const bounceTransition = {
-    // start in a transparent, upscaled state
     atEnter: {
       opacity: 0.5,
-      scale: bounce(0.8),
+      scale: bounce(10),
     },
-    // leave in a transparent, downscaled state
     atLeave: {
       opacity: bounce(0),
       scale: bounce(0.8),
     },
-    // and rest at an opaque, normally-scaled state
     atActive: {
       opacity: bounce(1),
+
       scale: bounce(1),
     },
   };
+
   return (
     <>
       <BrowserRouter>
@@ -64,7 +62,7 @@ function App() {
           atActive={bounceTransition.atActive}
           mapStyles={mapStyles}
         >
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+          <Route path="/" exact component={Home} />
           <Route path="/register" component={Register} />
           <Route path="/about" component={About} />
           <Route path="/create-activity" component={ActivitiesForm} />
@@ -72,11 +70,6 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/activities" component={ActivitiesList} />
           <Route path="/backoffice/Slides" component={SlidesForm} />
-          <Route
-            path="/backoffice/organization/edit"
-            component={EditOrganization}
-          />
-          <Route exact path="/backoffice/users" component={UsersList} />
           <Route
             path="/backoffice/organization/edit"
             component={EditOrganization}
