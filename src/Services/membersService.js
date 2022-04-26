c0ab290b859190e467bc90f37f344fa7314933d3
gomezdn <gomezdn@outlook.com>
2022-04-25 23:45:21 -0300

    created and used env variables for members http service

--- src/Services/membersService.js
+++ src/Services/membersService.js
@@ -1,22 +1,30 @@
 import { post, get, patch } from './privateApiService';
 
 const BASE_URL = 'https://ongapi.alkemy.org/api';
-const BASE_ENDPOINT = '/members';
 
 async function getMembers() {
-  return await get(`${BASE_URL}${BASE_ENDPOINT}`);
+  return await get(`${BASE_URL}${process.env.REACT_APP_GET_MEMBERS_API}`);
 }
 
 async function getMember(id) {
-  return await get(`${BASE_URL}${BASE_ENDPOINT}`, id);
+  return await get(
+    `${BASE_URL}${process.env.REACT_APP_GET_MEMBER_BY_ID_API}`,
+    id
+  );
 }
 
 async function createMember(memberDetails) {
-  return await post(`${BASE_URL}${BASE_ENDPOINT}`, memberDetails);
+  return await post(
+    `${BASE_URL}${process.env.REACT_APP_CREATE_MEMBER_API}`,
+    memberDetails
+  );
 }
 
 async function editMember(memberDetails) {
-  return await patch(`${BASE_URL}${BASE_ENDPOINT}`, memberDetails);
+  return await patch(
+    `${BASE_URL}${process.env.REACT_APP_EDIT_MEMBER_API}`,
+    memberDetails
+  );
 }
 
 export { getMembers, getMember, createMember, editMember };
