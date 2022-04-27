0b3257f5b2fd253e96aaa12f1a23676428dc6630
Alex Garcia <alexgarciaalcuadrado@gmail.com>
2022-04-27 16:35:58 -0300

    logogut button added

--- src/Components/Header/BackofficeHeader.js
+++ src/Components/Header/BackofficeHeader.js
@@ -1,7 +1,17 @@
-import React  from "react";
+import React from "react";
+import { useNavigate  } from "react-router-dom";
 import { Link } from "react-router-dom";
+import {logout} from "../../features/users/authReducer"
+import { useDispatch } from "react-redux";
 
-const BackofficeHeader = () => {   
+const BackofficeHeader = () => { 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    dispatch(logout);
+    navigate("/")
+  }
     return(
         <nav class="navbar-light bg-light d-flex justify-content-around align-items-start position-absolute top-0 start-0 h-100">
           <div>
@@ -12,6 +22,7 @@ const BackofficeHeader = () => {
                 <Link class="nav-link link-danger border-bottom" to="/create-activity">Crear actividad</Link>
                 <Link class="nav-link link-danger border-bottom" to="/create-member">Crear admin</Link>
                 <Link class="nav-link link-danger border-bottom" to="/backoffice/organization/edit">Organización</Link>
+                <button className="btn btn-danger mt-1 ms-1" onClick={logoutHandler}>Log out</button>
               </div>
             </div>
           </div>
